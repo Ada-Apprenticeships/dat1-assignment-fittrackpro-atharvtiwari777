e2e60bb672ef1bc27fdeb250640f57ee1a7ee326
Atharv Tiwari <atharv.tiwari@ada.ac.uk>
2026-02-24 15:41:13 +0000

    task 3 done

--- src/queries/3_equipment_management.sql
+++ src/queries/3_equipment_management.sql
@@ -2,10 +2,16 @@
 .mode column
 
 -- 3.1 
-
+SELECT equipment_id, name, next_maintenance_date
+FROM equipment
+WHERE next_maintenance_date BETWEEN '2025-01-01' AND '2025-01-30';
 
 -- 3.2 
-
+SELECT type AS equipment_type, COUNT(*) AS count
+FROM equipment
+GROUP BY type;
 
 -- 3.3 
-
+SELECT type AS equipment_type, AVG((julianday('now') - julianday(purchase_date))) AS avg_age_days
+FROM equipment
+GROUP BY type;
